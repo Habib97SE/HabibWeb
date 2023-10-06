df8439e2bbd637f512ec24bcefd89f7e61a58944
Habiballah Heazrehee <hahh20@student.bth.se>
2023-10-06 20:23:22 +0200

    Added blog management, profile to the admin area

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const session = require("express-session");
 const bcrypt = require("bcrypt");
 const router = express.Router();
 const model = require("../models/admin.js");
 
 router.get("/admin", async (req, res) => {
   // check if cookie is set
-  if (req.session && req.session.user && req.cookies.user_sid) {
+  if (req.session && req.session.user) {
     res.render("admin/index.handlebars", {
       layout: false,
       header: {
@@ -15,6 +14,13 @@ router.get("/admin", async (req, res) => {
         description: "Admin panel",
       },
       user: req.session.user,
+      footer: {
+        year: new Date().getFullYear(),
+        site: {
+          name: "HabibDev.",
+          url: "/",
+        },
+      },
     });
   } else {
     res.redirect("/admin/login");
@@ -74,6 +80,227 @@ router.post("/admin/login", async (req, res) => {
       email: emailAddress,
       password: password,
     });
+  }
+});
+
+router.get("/admin/profile", async (req, res) => {
+  // check if cookie is set
+  if (req.session && req.session.user) {
+    res.render("admin/profile.handlebars", {
+      layout: false,
+      header: {
+        title: "Profile",
+        keywords: "profile, admin",
+        description: "Admin profile",
+      },
+      user: req.session.user,
+      registeredSince: new Date(req.session.user.created_at).toDateString(),
+      footer: {
+        year: new Date().getFullYear(),
+        site: {
+          name: "HabibDev.",
+          url: "/",
+        },
+      },
+    });
+  } else {
+    res.redirect("/admin/login");
+  }
+});
+
+router.post("/admin/profile", async (req, res) => {
+  // if user is not logged in, redirect to login page
+  if (!req.session || !req.session.user) {
+    res.redirect("/admin/login");
+  }
+
+  // if password field is empty, redirect to profile page
+  if (req.body.password === "") {
+    res.redirect("/admin/profile");
+  }
+
+  const { firstName, lastName, emailAddress, password } = req.body;
+
+  // hash password using sha256
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const updated_user = {
+    admin_id: req.session.user.admin_id,
+    first_name: firstName,
+    last_name: lastName,
+    email_address: emailAddress,
+    password: hashedPassword,
+    created_at: req.session.user.created_at,
+    updated_at: new Date().toISOString(),
+  };
+  console.dir(updated_user);
+  // update user
+  const result = await model.updateUser(updated_user);
+
+  if (result) {
+    // update session
+    req.session.user.first_name = firstName;
+    req.session.user.last_name = lastName;
+    req.session.user.email_address = emailAddress;
+    res.redirect("/admin/profile");
+  } else {
+    res.redirect("/admin/profile");
+  }
+});
+
+/**
+ * @route GET /admin/blogs
+ * @description Get blogs
+ * @access Private
+ * @param {Number} limit - Limit
+ * @param {Number} offset - Offset
+ *
+ * @returns {Object} Blogs
+ */
+router.get("/admin/blogs", async (req, res) => {
+  let limit = 9;
+  let offset = 0;
+  // check if cookie is set
+  if (req.session && req.session.user) {
+    res.render("admin/blogs.handlebars", {
+      layout: false,
+      header: {
+        title: "Blogs",
+        keywords: "blogs, admin",
+        description: "Admin blogs",
+      },
+      user: req.session.user,
+      blogs: await model.getBlogs(limit, offset),
+      hasMultiplePages: true,
+      prev: 1,
+      next: 3,
+      currentPage: 2,
+      totalPages: 5,
+      footer: {
+        year: new Date().getFullYear(),
+        site: {
+          name: "HabibDev.",
+          url: "/",
+        },
+      },
+    });
+  } else {
+    res.redirect("/admin/login");
+  }
+});
+
+router.get("/admin/blogs/delete/:id", async (req, res) => {
+  // check if cookie is set
+  if (req.session && req.session.user) {
+    const id = req.params.id;
+    const result = await model.deleteBlog(id);
+    if (result) {
+      res.redirect("/admin/blogs");
+    } else {
+      res.redirect("/admin/blogs");
+    }
+  } else {
+    res.redirect("/admin/login");
+  }
+});
+
+router.get("/admin/blogs/edit/:id", async (req, res) => {
+  // check if cookie is set
+  if (req.session && req.session.user) {
+    const id = req.params.id;
+    const result = await model.getBlog(id);
+    if (result) {
+      res.render("admin/edit_blog.handlebars", {
+        layout: false,
+        header: {
+          title: "Edit Blog",
+          keywords: "edit, blog, admin",
+          description: "Edit blog",
+        },
+        user: req.session.user,
+        blog: result,
+        footer: {
+          year: new Date().getFullYear(),
+          site: {
+            name: "HabibDev.",
+            url: "/",
+          },
+        },
+      });
+    } else {
+      res.redirect("/admin/blogs");
+    }
+  } else {
+    res.redirect("/admin/login");
+  }
+});
+
+router.post("/admin/blogs/edit/:id", async (req, res) => {
+  // check if cookie is set
+  if (req.session && req.session.user) {
+    const id = req.params.id;
+    const { title, content } = req.body;
+    const blog = {
+      title: title,
+      content: content,
+      updated_at: new Date().toISOString(),
+    };
+    const result = await model.updateBlog(id, blog);
+    if (result) {
+      res.redirect("/admin/blogs");
+    } else {
+      res.redirect("/admin/blogs/edit/" + id);
+    }
+  } else {
+    res.redirect("/admin/login");
+  }
+});
+
+router.get("/admin/blogs/new", async (req, res) => {
+  // check if cookie is set
+  if (req.session && req.session.user) {
+    res.render("admin/new_post.handlebars", {
+      layout: false,
+      header: {
+        title: "Add Blog",
+        keywords: "add, blog, admin",
+        description: "Add blog",
+      },
+      user: req.session.user,
+      footer: {
+        year: new Date().getFullYear(),
+        site: {
+          name: "HabibDev.",
+          url: "/",
+        },
+      },
+    });
+  } else {
+    res.redirect("/admin/login");
+  }
+});
+
+router.post("/admin/blog/add", async (req, res) => {
+  // check if cookie is set
+  if (req.session && req.session.user) {
+    const { title, content, main_image } = req.body;
+    const blog = {
+      author_id = req.session.user.admin_id,
+      title: title,
+      content: content,
+      main_image: main_image,
+      created_at: new Date().toISOString(),
+      updated_at: new Date().toISOString(),
+    };
+    const result = await model.addBlog(blog);
+    if (result) {
+      res.redirect("/admin/blogs");
+    } else {
+      res.redirect("/admin/blogs/new");
+    }
+  } else {
+    res.redirect("/admin/login");
   }
 });
 
--- models/admin.js
+++ models/admin.js
@@ -24,3 +24,77 @@ module.exports.getUser = (emailAddress) => {
     );
   });
 };
+
+module.exports.updateUser = (user) => {
+  return new Promise((resolve, reject) => {
+    db.run(
+      "UPDATE Admins SET first_name = ?, last_name = ?, email_address = ?, password = ?, updated_at = ? WHERE admin_id = ?",
+      [
+        user.first_name,
+        user.last_name,
+        user.email_address,
+        user.password,
+        user.updated_at,
+        user.admin_id,
+      ],
+      (err) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(true);
+      }
+    );
+  });
+};
+
+module.exports.getBlogs = (limit, offset) => {
+  return new Promise((resolve, reject) => {
+    db.all(
+      "SELECT * FROM Blogs LIMIT ? OFFSET ?",
+      [limit, offset],
+      (err, rows) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(rows);
+      }
+    );
+  });
+};
+
+module.exports.deleteBlog = (blogId) => {
+  return new Promise((resolve, reject) => {
+    db.run("DELETE FROM Blogs WHERE blog_id = ?", [blogId], (err) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(true);
+    });
+  });
+};
+
+module.exports.getBlog = (blogId) => {
+  return new Promise((resolve, reject) => {
+    db.get("SELECT * FROM Blogs WHERE blog_id = ?", [blogId], (err, row) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(row);
+    });
+  });
+};
+
+module.exports.updateBlog = (blog) => {
+  return new Promise((resolve, reject) => {
+    db.run(
+      "UPDATE Blogs SET title = ?, content = ?, updated_at = ? WHERE blog_id = ?",
+      [blog.title, blog.content, blog.updated_at, blog.blog_id],
+      (err) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(true);
+      }
+    );
+  });
+};
